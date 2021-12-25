4e1e25dc226f6e844334690b60c7e2433ffe1bf4
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2021-12-25 08:13:27 -0800

    Update to GUI for Mac OS compatibility

--- processor.py
+++ processor.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 from tkinter.ttk import Frame
 from tkinter import filedialog, messagebox
 import cv2 as cv
@@ -11,21 +12,23 @@ class MainApplication(Frame):
     def __init__(self):
         super().__init__()
         self.configure_gui()
-        self.addVariablesToFrame()
-        self.addLabelsToFrame()
-        self.addButtonsToFrame()
-        self.addCheckbuttonsToFrame()
-        self.addRadiobuttonsToFrame()
-        self.addInputsToFrame()
+        self.add_variables()
+        self.add_labels()
+        self.add_buttons()
+        self.add_checkbuttons()
+        self.add_radiobuttons()
+        self.add_input()
+        self.add_slider()
+        self.place_widgets()
 
     def configure_gui(self):
         self.winfo_toplevel().title("Bulk Image Processor")
-        self.master.geometry("395x185")
+        self.master.geometry("480x255")
         self.master.resizable(False, False)
         self.icon = tk.PhotoImage(file="icon.png")
         self.master.iconphoto(False, self.icon)
 
-    def addVariablesToFrame(self):
+    def add_variables(self):
         self.var_resize = tk.StringVar(self.master, 'False')
         self.var_flip = tk.StringVar(self.master, 'False')
         self.var_rotate = tk.StringVar(self.master, 'False')
@@ -38,99 +41,80 @@ class MainApplication(Frame):
         self.var_orient = tk.StringVar(self.master, '1')
         self.var_thresh = tk.StringVar(self.master, 'False')
         self.var_edges = tk.StringVar(self.master, 'False')
+        self.var_slider = tk.StringVar(root, '')
         self.count = 1
         self.image = ''
         self.source = ''
         self.dest = ''
 
-    def addLabelsToFrame(self):
+    def add_labels(self):
         self.lbl_scale = tk.Label(root, text='Scale: ', state='disabled')
         self.lbl_format = tk.Label(root, text='Choose Format: ')
-        self.lbl_src = tk.Label(root, text='SOURCE: ')
-        self.lbl_dest = tk.Label(root, text='DESTINATION: ')
-        self.lbl_scale.place(x=5, y=30)
-        self.lbl_format.place(x=265, y=5)
-        self.lbl_src.place(x=5, y=135)
-        self.lbl_dest.place(x=5, y=160)
 
-    def addButtonsToFrame(self):
+    def add_buttons(self):
         self.btn_source = tk.Button(root, text='Source',
                                     command=self.ifSourceClicked)
 
         self.btn_dest = tk.Button(root, text='Destination',
                                   command=self.ifDestClicked)
 
-        self.btn_create = tk.Button(root, text='      Create Images      ',
+        self.btn_create = tk.Button(root, text='Create Files',
                                     command=self.ifCreateClicked)
 
-        self.btn_source.place(x=265, y=75)
-        self.btn_dest.place(x=315, y=75)
-        self.btn_create.place(x=265, y=105)
+    def add_checkbuttons(self):
+        self.chk_resize = tk.Checkbutton(root, text='Resize', 
+            variable=self.var_resize, command=self.ifResizeChecked)
 
-    def addCheckbuttonsToFrame(self):
-        self.chk_resize = tk.Checkbutton(root, text='Resize', variable=self.var_resize,
-                                         command=self.ifResizeChecked)
+        self.chk_flip = tk.Checkbutton(root, text='Flip', 
+            variable=self.var_flip, command=self.ifFlipChecked)
 
-        self.chk_flip = tk.Checkbutton(root, text='Flip', variable=self.var_flip,
-                                       command=self.ifFlipChecked)
+        self.chk_rotate = tk.Checkbutton(root, text='Rotate Clockwise', 
+            variable=self.var_rotate, command=self.ifRotateChecked)
 
-        self.chk_rotate = tk.Checkbutton(root, text='Rotate Clockwise', variable=self.var_rotate,
-                                         command=self.ifRotateChecked)
+        self.chk_sharpen = tk.Checkbutton(root, text='Sharpen', 
+            variable=self.var_sharpen)
 
-        self.chk_sharpen = tk.Checkbutton(
-            root, text='Sharpen', variable=self.var_sharpen)
-        self.chk_blur = tk.Checkbutton(
-            root, text='Gaussian Blur', variable=self.var_blur)
-        self.chk_gray = tk.Checkbutton(
-            root, text='Grayscale', variable=self.var_gray)
-        self.chk_thresh = tk.Checkbutton(
-            root, text='Thresholding', variable=self.var_thresh)
-        self.chk_edge = tk.Checkbutton(
-            root, text='Edge Detection', variable=self.var_edges)
+        self.chk_blur = tk.Checkbutton(root, text='Gaussian Blur', 
+            variable=self.var_blur, command=self.ifBlurChecked)
 
-        self.chk_resize.place(x=5, y=5)
-        self.chk_flip.place(x=5, y=50)
-        self.chk_rotate.place(x=5, y=90)
-        self.chk_sharpen.place(x=155, y=5)
-        self.chk_blur.place(x=155, y=30)
-        self.chk_gray.place(x=155, y=55)
-        self.chk_thresh.place(x=155, y=80)
-        self.chk_edge.place(x=155, y=105)
+        self.chk_gray = tk.Checkbutton(root, text='Grayscale', 
+            variable=self.var_gray)
 
-    def addRadiobuttonsToFrame(self):
+        self.chk_thresh = tk.Checkbutton(root, text='Thresholding', 
+            variable=self.var_thresh)
+
+        self.chk_edge = tk.Checkbutton(root, text='Edge Detection', 
+            variable=self.var_edges)
+
+    def add_radiobuttons(self):
         self.rb_vertical = tk.Radiobutton(root, text='Vertical',
                                           variable=self.var_orient, value='1', state='disabled')
 
-        self.rb_horizontal = tk.Radiobutton(root, text='Horizontal',
-                                            variable=self.var_orient, value='2', state='disabled')
+        self.rb_horizontal = tk.Radiobutton(root, text='Horizontal', 
+            variable=self.var_orient, value='2', state='disabled')
 
-        self.rb_90 = tk.Radiobutton(root, text='90°', variable=self.var_degrees,
-                                    value='1', state='disabled')
+        self.rb_90 = tk.Radiobutton(root, text='90°', 
+            variable=self.var_degrees, value='1', state='disabled')
 
-        self.rb_180 = tk.Radiobutton(root, text='180°', variable=self.var_degrees,
-                                     value='2', state='disabled')
+        self.rb_180 = tk.Radiobutton(root, text='180°', 
+            variable=self.var_degrees, value='2', state='disabled')
 
-        self.rb_270 = tk.Radiobutton(root, text='270°', variable=self.var_degrees,
-                                     value='3', state='disabled')
+        self.rb_270 = tk.Radiobutton(root, text='270°', 
+            variable=self.var_degrees, value='3', state='disabled')
 
-        self.rb_png = tk.Radiobutton(root, text='PNG', variable=self.var_format,
-                                     value='1')
+        self.rb_png = tk.Radiobutton(root, text='PNG', 
+            variable=self.var_format, value='1')
 
-        self.rb_jpg = tk.Radiobutton(root, text='JPG', variable=self.var_format,
-                                     value='2')
+        self.rb_jpg = tk.Radiobutton(root, text='JPG', 
+            variable=self.var_format, value='2')
 
-        self.rb_vertical.place(x=5, y=70)
-        self.rb_horizontal.place(x=70, y=70)
-        self.rb_90.place(x=5, y=110)
-        self.rb_180.place(x=50, y=110)
-        self.rb_270.place(x=95, y=110)
-        self.rb_png.place(x=270, y=25)
-        self.rb_jpg.place(x=270, y=45)
-
-    def addInputsToFrame(self):
+    def add_input(self):
         self.inp_scale = tk.Entry(
             root, width=5, textvariable=self.var_scale, state='disabled')
-        self.inp_scale.place(x=43, y=30)
+
+    def add_slider(self):
+        self.slider = Scale(root, variable = self.var_slider, 
+           from_ = 1, to = 3, orient = HORIZONTAL, state='disabled') 
 
     def ifResizeChecked(self):
 
@@ -162,6 +146,50 @@ class MainApplication(Frame):
             self.rb_180['state'] = 'disabled'
             self.rb_270['state'] = 'disabled'
 
+    def ifBlurChecked(self):
+
+        if self.slider['state'] == 'disabled':
+            self.slider['state'] = 'normal'
+        else:
+            self.slider['state'] = 'disabled'
+            self.var_slider.set('1')
+
+    def place_widgets(self):
+
+        # RADIO BUTTONS
+        self.rb_vertical.place(x=25, y=130)
+        self.rb_horizontal.place(x=100, y=130)
+        self.rb_90.place(x=25, y=205)
+        self.rb_180.place(x=75, y=205)
+        self.rb_270.place(x=130, y=205)
+        self.rb_png.place(x=365, y=80)
+        self.rb_jpg.place(x=365, y=50)
+
+        # SLIDER
+        self.slider.place(x=210, y=80)
+
+        # INPUT
+        self.inp_scale.place(x=68, y=55)
+
+        # CHECK BUTTONS      
+        self.chk_resize.place(x=25, y=25)
+        self.chk_flip.place(x=25, y=100)
+        self.chk_rotate.place(x=25, y=175)
+        self.chk_sharpen.place(x=210, y=25)
+        self.chk_blur.place(x=210, y=60)
+        self.chk_gray.place(x=210, y=135)
+        self.chk_thresh.place(x=210, y=165)
+        self.chk_edge.place(x=210, y=200)
+
+        # LABELS
+        self.lbl_scale.place(x=25, y=55)
+        self.lbl_format.place(x=365, y=25)
+
+        # BUTTONS       
+        self.btn_source.place(x=365, y=120)
+        self.btn_dest.place(x=365, y=160)
+        self.btn_create.place(x=365, y=200)
+
     def resize(self):
         width = int(self.image.shape[1] * float(self.var_scale.get()))
         height = int(self.image.shape[0] * float(self.var_scale.get()))
@@ -188,7 +216,18 @@ class MainApplication(Frame):
         self.image = cv.filter2D(image, -1, kernel)
 
     def blur(self, image):
-        self.image = cv.GaussianBlur(image, (7, 7), cv.BORDER_DEFAULT)
+        k_size = int(self.var_slider.get())
+
+        if k_size == 1:
+            k_size = (5, 5)
+
+        if k_size == 2:
+            k_size = (7, 7)
+
+        if k_size == 3:
+            k_size = (9, 9)
+
+        self.image = cv.GaussianBlur(image, k_size, cv.BORDER_DEFAULT)
 
     def gray(self, image):
         self.image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -203,16 +242,8 @@ class MainApplication(Frame):
     def ifSourceClicked(self):
         self.source = filedialog.askdirectory()
 
-        if len(self.source) != 0:
-            self.lbl_src.config(text="SOURCE: " + self.source)
-            self.lbl_src.place(x=5, y=135)
-
     def ifDestClicked(self):
         self.dest = filedialog.askdirectory()
-
-        if len(self.dest) != 0:
-            self.lbl_dest.config(text="DESTINATION: " + self.dest)
-            self.lbl_dest.place(x=5, y=160)
 
     def ifCreateClicked(self):
 
@@ -309,7 +340,7 @@ class MainApplication(Frame):
 
         finish = time.perf_counter()
 
-        status = f'{self.count-1} image(s) processed in {round(finish-start, 2)} second(s)'
+        status = f'{self.count-1} image(s) created in {round(finish-start, 2)} second(s)'
         messagebox.showinfo('Bulk Image Processor', status)
         self.count = 1
 
